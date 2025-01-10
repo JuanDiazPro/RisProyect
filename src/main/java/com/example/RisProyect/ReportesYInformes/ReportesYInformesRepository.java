b6e1a44265b63ceaecff2d31754c73606f751e2f
AngelOcampo007 <20233tn058@utez.edu.mx>
2025-01-09 21:29:09 -0600

    agregue mis clases

---
+++ src/main/java/com/example/RisProyect/ReportesYInformes/ReportesYInformesRepository.java
@@ -0,0 +1,7 @@
+package com.example.RisProyect.ReportesYInformes;
+
+import com.example.RisProyect.ReportesYInformes.model.ReportesYInformes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportesYInformesRepository extends JpaRepository<ReportesYInformes, Long> {
+}
