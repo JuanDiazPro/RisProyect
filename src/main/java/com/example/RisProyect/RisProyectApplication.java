0eb1834a72c640295b409c7668e6fb647792793b
Juan Diaz <20233tn067@utez.edu.mx>
2025-01-09 20:55:38 -0600

    Se agrega proyecto

---
+++ src/main/java/com/example/RisProyect/RisProyectApplication.java
@@ -0,0 +1,13 @@
+package com.example.RisProyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RisProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RisProyectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RisProyect/RisProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RisProyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RisProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
